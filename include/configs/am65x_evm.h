e8b09eb99ccbded38f5f4be9f60b475274b6ecd2
Lokesh Vutla <lokeshvutla@ti.com>
2018-06-07 14:33:55 +0530

    board: ti: am654: a53: Add initial support for am654
    Add initial support for AM654 based EVM running on A53. Enable
    4GB of DDR available on the EVM so that kernel DTB file
    can be updated accordingly.
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
    [Andreas: Added 4GB ddr support]
    Signed-off-by: Andreas Dannenberg <dannenberg@ti.com>
    
    board: ti: am654: Enable full 4GB of DDR available on EVM
    
    In order to be able to use the full 4GB of DDR available on the AM654
    EVM we need to make U-Boot aware of this so that the Kernel DTB file
    can get updated accordingly. Since our boot process is split across
    32-bit (R5) and 64-bit (A53) cores the simplest way to accomplish this
    is to only configure the portion of U-Boot executing on the A53 for
    the full amount of DDR memory available as follows:
    
    Bank #0: 2 GiB @ 0x080000000
    Bank #1: 2 GiB @ 0x880000000
    
    Note that independent of the CPU core the DDR usage by U-Boot itself
    is limited to first bank which comprises the lower memory DDR region.
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

---
+++ include/configs/am65x_evm.h
@@ -0,0 +1,36 @@
+/*
+ * Configuration header file for K3 AM654 EVM
+ *
+ * Copyright (C) 2017-2018 Texas Instruments Incorporated - http://www.ti.com/
+ *	Lokesh Vutla <lokeshvutla@ti.com>
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef __CONFIG_AM654_EVM_H
+#define __CONFIG_AM654_EVM_H
+
+#include <linux/sizes.h>
+#include <config_distro_bootcmd.h>
+
+#define CONFIG_ENV_SIZE			(128 << 10)
+
+/* DDR Configuration */
+#define CONFIG_NR_DRAM_BANKS		2
+#define CONFIG_SYS_SDRAM_BASE1		0x880000000
+
+/* SPL Loader Configuration */
+#ifdef CONFIG_TARGET_AM654_A53_EVM
+#define CONFIG_SPL_TEXT_BASE		0x80080000
+#endif
+
+#define CONFIG_SKIP_LOWLEVEL_INIT
+
+#define CONFIG_SPL_MAX_SIZE		CONFIG_MAX_DOWNLODABLE_IMAGE_SIZE
+#define CONFIG_SYS_INIT_SP_ADDR         (CONFIG_SPL_TEXT_BASE +	\
+					CONFIG_NON_SECURE_MSRAM_SIZE - 4)
+
+/* Now for the remaining common defines */
+#include <configs/ti_armv7_common.h>
+
+#endif /* __CONFIG_AM654_EVM_H */
