4e7574ef28367b144a0d4d2ab1cfa36437c452e0
Lokesh Vutla <lokeshvutla@ti.com>
2019-09-24 13:07:15 +0530

    power: domain: Introduce dev_power_domain_off
    Add dev_power_domain_off() api to disable all the power-domains
    corresponding to a device
    
    Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>

--- drivers/power/domain/power-domain-uclass.c
+++ drivers/power/domain/power-domain-uclass.c
@@ -107,10 +107,10 @@ int power_domain_off(struct power_domain *power_domain)
 	return ops->off(power_domain);
 }
 
-int dev_power_domain_on(struct udevice *dev)
+static int dev_power_domain_ctrl(struct udevice *dev, bool on)
 {
 	struct power_domain pd;
-	int i, count, ret;
+	int i, count, ret = 0;
 
 	count = dev_count_phandle_with_args(dev, "power-domains",
 					    "#power-domain-cells");
@@ -118,12 +118,23 @@ int dev_power_domain_on(struct udevice *dev)
 		ret = power_domain_get_by_index(dev, &pd, i);
 		if (ret)
 			return ret;
-		ret = power_domain_on(&pd);
-		if (ret)
-			return ret;
+		if (on)
+			ret = power_domain_on(&pd);
+		else
+			ret = power_domain_off(&pd);
 	}
 
-	return 0;
+	return ret;
+}
+
+int dev_power_domain_on(struct udevice *dev)
+{
+	return dev_power_domain_ctrl(dev, true);
+}
+
+int dev_power_domain_off(struct udevice *dev)
+{
+	return dev_power_domain_ctrl(dev, false);
 }
 
 UCLASS_DRIVER(power_domain) = {
